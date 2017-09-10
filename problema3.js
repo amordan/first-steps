cea07e61962f182dc17c14b63e9582e0853b38af
Cesar <ccdl15c@gmail.com>
2017-09-10 18:42:40 -0400

    assigment 3 for completion

---
+++ problema3.js
@@ -0,0 +1,18 @@
+/**
+ * Tienes 100 manzanas de distintos colors (amarillas, rojas y verdes). Sacar 
+ * el porcentaje correspondiente para cada tipo de manzana.
+ * 
+ * - Amarillas -> 30
+ * - Rojas -> 60
+ * - Verdes -> 10
+ * 
+ * Al finalizar imprimras el resultado de la siguiente forma:
+ * 
+ * Pct. Amarillas = x%
+ * Pct. Rojas = x%
+ * Pct. Verdes = x%
+ */
+
+ console.log(pctAmarillas + '%');
+ console.log(pctRojas + '%');
+ console.log(pctVerdes + '%');
